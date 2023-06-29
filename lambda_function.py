51d63f6b444240fcbeb026602a58acb27b7a81ef
mongoanaiya <86017775+mongoanaiya@users.noreply.github.com>
2023-06-29 12:31:17 -0400

    first commit

---
+++ lambda_function.py
@@ -0,0 +1,27 @@
+import os
+from pymongo import MongoClient
+
+client = MongoClient(host=os.environ.get("ATLAS_URI"))
+
+def lambda_handler(event, context):
+    # Choose our "sample_restaurants" database and our "restaurants" collection
+    db = client.sample_restaurants
+    collection = db.restaurants
+
+    # This is our aggregation pipeline
+    pipeline = [
+        # We are finding American restaurants in Brooklyn
+        {"$match": {"borough": "Brooklyn", "cuisine": "American"}},
+        # We only want 5 out of our over 20k+ documents
+        {"$limit": 5},
+        # We don't want all the details, project what you need
+        {"$project": {"_id": 0, "borough": 1, "cuisine": 1, "name": 1}}
+    ]
+
+    # This will show our pipeline 
+    result = list(collection.aggregate(pipeline))
+
+    # Print the result
+    for restaurant in result:
+        print(restaurant)
+
